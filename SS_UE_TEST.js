d6d36ed3c8eee299f4a5ecb6672d28be12f9cfb8
WasiqueCeligo957 <wasiqur.rahman@celigo.com>
2025-11-28 23:25:50 +0530

    file modified

--- SS_UE_TEST.js
+++ SS_UE_TEST.js
@@ -5,12 +5,12 @@
 define(['N/record', 'N/log'], (record, log) => {
     const afterSubmit = (context) => {
         const get_record = context.newRecord;
-        const record_type=get_record.type;
+        const record_type = get_record.type;
         log.debug('get_record', `record-:${get_record}`);
         log.debug('record type', `${record_type}`);
 
     }
     return {
-        afterSubmit: afterSubmit
+        afterSubmit
     }
 })
