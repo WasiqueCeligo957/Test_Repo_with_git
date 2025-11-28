2d4cb810f119a3764945d55a2f2bcac02a3148f2
WasiqueCeligo957 <wasiqur.rahman@celigo.com>
2025-11-28 23:49:43 +0530

    Update SS_UE_TEST.js
    Changes updated in main

--- SS_UE_TEST.js
+++ SS_UE_TEST.js
@@ -25,15 +25,8 @@ define(['N/record', 'N/log'], (record, log) => {
     const afterSubmit = (context) => {
         const get_record = context.newRecord;
         const record_type = get_record.type;
-        const record_id = get_record.id;
         log.debug('get_record', `record-:${get_record}`);
         log.debug('record type', `${record_type}`);
-        log.debug('record id', `${record_id}`);
-        const load_record = get_record.load({
-            type: 'record_type',
-            id: record_type
-        });
-        log.debug('record loaded', `${load_record}`);
 
 
     }
