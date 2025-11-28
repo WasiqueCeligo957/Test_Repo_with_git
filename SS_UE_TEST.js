3bc81c661049747ef406b9fdbd59617df96426f5
WasiqueCeligo957 <wasiqur.rahman@celigo.com>
2025-11-28 23:48:18 +0530

    Update SS_UE_TEST.js

--- SS_UE_TEST.js
+++ SS_UE_TEST.js
@@ -3,24 +3,6 @@
  * @NScriptType UserEventScript
  */
 define(['N/record', 'N/log'], (record, log) => {
-    //add button on record in view mode
-    const beforeLoad = (context) => {
-        try {
-            if (context.type === context.Type.UserEventType.VIEW) {
-                const form = context.form;
-                form.addButton({
-                    id: 'custpage_cust_bttn',
-                    label: 'click to send',
-                    functionname: 'myfunction'
-                });
-                //add client script to controll the button
-                form.clientScriptModulePath = 'SuiteScripts/cleint_script_file_id';
-
-            }
-        } catch (error) {
-            log.debug('capture error message', `${error.message}`);
-        }
-    }
     //aftersubmit function to do some activity
     const afterSubmit = (context) => {
         const get_record = context.newRecord;
