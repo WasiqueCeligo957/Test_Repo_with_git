fe0d2859f1edbb83c7d647a45df602c66a2dba89
WasiqueCeligo957 <wasiqur.rahman@celigo.com>
2025-12-01 12:26:44 +0530

    script file modified on 01.12.2025

--- SS_UE_TEST.js
+++ SS_UE_TEST.js
@@ -14,7 +14,7 @@ define(['N/record', 'N/log'], (record, log) => {
                     functionname: 'myfunction'
                 });
                 //add client script to controll the button
-               // form.clientScriptModulePath = 'SuiteScripts/cleint_script_file_id';
+                // form.clientScriptModulePath = 'SuiteScripts/cleint_script_file_id';
 
             }
         } catch (error) {
@@ -23,23 +23,34 @@ define(['N/record', 'N/log'], (record, log) => {
     }
     //aftersubmit function to do some activity
     const afterSubmit = (context) => {
-        try{
-        const get_record = context.newRecord;
-        const record_type = get_record.type;
-        const record_id = get_record.id;
-        log.debug('get_record', `record-:${get_record}`);
-        log.debug('record type', `${record_type}`);
-        log.debug('record id', `${record_id}`);
-        const load_record = record.load({
-            type: record_type,
-            id: record_id
-        });
-        log.debug('record loaded', `${load_record}`);
-        const get_entity=load_record.getvalue({fieldId:'entity'});
-        log.debug('customer Id',`${get_entity}`);
-    }catch(e){
-        error.log('error', `collect error- ${e.message}`);
-    }
+        try {
+            const get_record = context.newRecord;
+            const record_type = get_record.type;
+            const record_id = get_record.id;
+            log.debug('get_record', `record-:${get_record}`);
+            log.debug('record type', `${record_type}`);
+            log.debug('record id', `${record_id}`);
+            const load_record = record.load({
+                type: record_type,
+                id: record_id
+            });
+            log.debug('record loaded', `${load_record}`);
+            const get_entity = load_record.getValue({ fieldId: 'entity' });
+            log.debug('customer Id', `${get_entity}`);
+            const line_count = load_record.getLineCount({ sublistId: 'item' });
+            log.debug('total lines', `${line_count}`);
+            for (let i = 0; i < line_count; i++) {
+                const itemID = load_record.getSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'item',
+                    line: i
+                });
+                log.debug('item id', `${itemID}`);
+            }
+        } catch (error) {
+            log.error('afterSubmit Error', `${error.message}`);
+        }
+
 
     }
     return {
